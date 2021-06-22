81306aa95fbad45100e0fc150c1b4b90e3eaa067
iwenzel.w <iwenzel.w@gmail>
2021-06-22 21:43:29 +0500

    урок 10

---
+++ js/script_2.js
@@ -0,0 +1,32 @@
+if (4 == 9) {
+  console.log('ok')
+} else {
+  console.log('Error')
+}
+
+const num = 51
+
+if (num < 49) {
+  console.log('Error')
+} else if (num > 100)  {
+  console.log('Много')
+} else {
+  console.log("OK!")
+}
+
+(num === 50) ? console.log('ok!') : console.log('error')
+
+switch (num) {
+  case 49:
+    console.log('неверно')
+    break;
+  case 100:
+    console.log('неверно')
+    break
+  case 50:
+    console.log('верно')
+    break
+  default:
+    console.log('в другой раз')
+    break
+}
