946c719e238f97654671a293798a31dc57006ad9
iwenzel.w <iwenzel.w@gmail>
2021-06-23 11:21:05 +0500

    задание 14

---
+++ js/script_14.js
@@ -0,0 +1,22 @@
+const str = "test"
+console.log(str)
+const rez = str.toUpperCase()
+console.log(rez)
+const rez2 = rez.toLowerCase()
+console.log(rez2)
+const fruit = "Some fruit"
+console.log(fruit.indexOf("uit"))
+
+const logg = "Hello World"
+console.log(logg.slice(6, 11))
+console.log(logg.substring(6, 11))
+console.log(logg.substr(6, 5))
+
+
+const num = 12.2
+console.log(Math.round(num))
+
+const test = "12.2px"
+console.log(parseInt(test))
+console.log(parseFloat(test))
+
